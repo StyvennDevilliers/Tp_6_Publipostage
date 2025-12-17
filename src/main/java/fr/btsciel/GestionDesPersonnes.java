d58435d46affc05de46ba362143a09a1c3b9a98d
StyvennDevilliers <styvenn.devilliers@lyceeastier.com>
2025-12-17 19:39:11 +0100

    ENFIN

--- src/main/java/fr/btsciel/GestionDesPersonnes.java
+++ src/main/java/fr/btsciel/GestionDesPersonnes.java
@@ -15,38 +15,45 @@ import java.util.Map;
 
 public class GestionDesPersonnes {
 
-    private Map<String, Object> m;
+    private Map<String, String> m;
+
 
     private static final Path path = Paths.get("contact.json");
 
     private static ArrayList<Person> person = new ArrayList<>();
 
     public void ecrirePerson() throws IOException {
-        for (Person value : person) {
-            //map.put(,value.getAdherent());
+        JsonElement tree = new JsonParser().parse(new FileReader("contact.json"));
+        Gson gson = new Gson();
+        StringBuilder modele = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader("lettreType.txt"));
 
-            BufferedReader br = new BufferedReader(new FileReader("lettreType.txt"));
-            BufferedWriter bw = new BufferedWriter(new FileWriter("lettre" + value.getNom() + ".txt"));
+        for(JsonElement e : tree.getAsJsonArray()) {
+            m = gson.fromJson(e, Map.class);
             while(br.ready()) {
                 String line = br.readLine();
-                bw.write(line);
-                bw.newLine();
+                modele.append(line).append("\n");
             }
+            br.close();
+
+            String lettre = modele.toString();
+            for (Map.Entry<String, String> entry : m.entrySet()) {
+                lettre = lettre.replace("[" + entry.getKey() + "]", entry.getValue());
+            }
+            lettre = lettre.replace("[dateDuJour]", java.time.LocalDate.now().toString());
+            BufferedWriter bw = new BufferedWriter(new FileWriter("lettre" + m.get("nom") + ".txt"));
+            bw.write(lettre);
             bw.close();
         }
     }
 
     private void lectureFichier() throws IOException {
-        JsonElement tree = new JsonParser().parse(new FileReader("contact.json"));
         if (Files.exists(path)) {
             FileReader fr = new FileReader(path.toFile());
             Type type = new TypeToken<ArrayList<Person>>(){}.getType();
             Gson gson = new Gson();
             person = gson.fromJson(fr,type);
-            for(JsonElement e : tree.getAsJsonArray()) {
-                m = gson.fromJson(e, Map.class);
-            }
-            System.out.println(m.get("nom"));
+
         }else{
             throw new FileNotFoundException("file not found");
         }
